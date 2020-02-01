80d365624c2cc47b275f065ffc60a9d42e96d0e6
Andrew <andrew.j.price@outlook.com>
2020-02-01 10:36:27 -0800

    Add isMovePossible for Pawn general use cases

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,19 +8,28 @@ export default class Game extends React.Component {
     super();
     this.state = {
       squares: initializeBoard(),
-      currentSelected: -1
+      currentSelected: -1,
+      player: 1
     }
   }
 
   movePiece(i) {
     const squares = this.state.squares.slice();
+
     if (this.state.currentSelected === -1) {
-      squares[i].style = {...squares[i].style, backgroundColor: 'gold'};
-      this.setState({ currentSelected: i });
-    } else if (this.state.currentSelected > -1) {
+      if (squares[i] && squares[i].player === this.state.player) {
+        squares[i].style = {...squares[i].style, backgroundColor: 'gold'};
+        this.setState({ currentSelected: i });
+      }
+    }
+    else if (this.state.currentSelected > -1) {
       squares[this.state.currentSelected].style = {...squares[this.state.currentSelected].style, backgroundColor: ''};
-      squares[i] = squares[this.state.currentSelected];
-      squares[this.state.currentSelected] = null;
+      if (squares[i] === null || squares[i].player !== this.state.player) {
+        if (squares[this.state.currentSelected].isMovePossible(this.state.currentSelected, i, squares[i])) {
+          squares[i] = squares[this.state.currentSelected];
+          squares[this.state.currentSelected] = null;
+        }
+      }
       this.setState({ squares: squares, currentSelected: -1});
     }
   }
--- src/pieces/Pawn.js
+++ src/pieces/Pawn.js
@@ -5,5 +5,26 @@ export class Pawn extends Piece {
         const light = "https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg";
         const dark = "https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg";
         super(player, (player === 1 ? light : dark));
+        this.player = player;
+        this.startingPosition = {
+            1: [48, 49, 50, 51, 52, 53, 54, 55],
+            2: [8, 9, 10, 11, 12, 13, 14, 15, 16]
+        }
+    }
+
+    isMovePossible(currentPosition, destination, destinationSquare) {
+        // Standard 1 move
+        if (currentPosition - destination === 8 && !destinationSquare) {
+            return true;
+        }
+        // 2 moves from start
+        else if (!destinationSquare && (currentPosition - destination === 16 && this.startingPosition[this.player].indexOf(currentPosition) !== -1)) {
+            return true;
+        }
+        // Piece capture
+        else if ((currentPosition - destination === 7 || currentPosition - destination === 9) && destinationSquare) {
+            return true;
+        }
+        return false;
     }
 }
