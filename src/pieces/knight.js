3cc421bc98f595f51725788ce295470dd31906b8
Andrew <andrew.j.price@outlook.com>
2020-02-02 09:40:48 -0800

    Add isMovePossible() logic for knight pieces

--- src/pieces/knight.js
+++ src/pieces/knight.js
@@ -5,5 +5,19 @@ export class Knight extends Piece {
         const light = "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg";
         const dark = "https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg";
         super(player, (player === 1 ? light : dark));
+        this.player = player;
+        this.validMoves = [6, 10, 15, 17];
+    }
+
+    isMovePossible(currentPosition, destination, destinationSquare) {
+        if (this.validMoves.indexOf(Math.abs(currentPosition - destination)) > -1) {
+            if (!destinationSquare) {
+                return true;
+            } else if (destinationSquare.player !== this.player) {
+                return true
+            }
+        } else {
+            return false;
+        }
     }
 }
