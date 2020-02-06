1fa3fc2712967bda686b2ab0aa10302e80bb6d00
Andrew <andrew.j.price@outlook.com>
2020-02-05 21:06:24 -0800

    Add Queen move logic

--- src/pieces/queen.js
+++ src/pieces/queen.js
@@ -5,5 +5,46 @@ export class Queen extends Piece {
         const light = "https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg";
         const dark = "https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg";
         super(player, (player === 1 ? light : dark));
+        this.player = player;
+    }
+
+    isMovePossible(currentPosition, destination, squares) {
+        return (Math.abs(currentPosition-destination) % 1 === 0 ||
+                Math.abs(currentPosition-destination) % 8 === 0 ||
+                Math.abs(currentPosition-destination) % 7 === 0 ||
+                Math.abs(currentPosition-destination) % 9 === 0)
+    }
+
+    movePath(start, end) {
+        var path = [];
+        let pathStart, pathEnd, increment;
+        if (start > end) {
+            pathStart = end;
+            pathEnd = start;
+        } else {
+            pathStart = start;
+            pathEnd = end;
+        }
+        if (Math.abs(start - end) % 7 === 0 ) {
+            increment = 7;
+            pathStart += increment;
+        }
+        else if (Math.abs(start - end) % 9 === 0 ) {
+            increment = 9;
+            pathStart += increment;
+        }
+        else if (Math.abs(start - end) % 8 === 0 ) {
+            increment = 8;
+            pathStart += increment;
+        }
+        else {
+            increment = 1;
+            pathStart += increment;
+        }
+
+        for (let i=pathStart; i<pathEnd; i+= increment) {
+            path.push(i);
+        }
+        return path;
     }
 }
