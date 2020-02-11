b43ce2929fc47a51f8b22a3db329a8f5b4842b28
Andrew <andrew.j.price@outlook.com>
2020-02-10 20:27:50 -0800

    Add Rook move logic

--- src/pieces/rook.js
+++ src/pieces/rook.js
@@ -6,4 +6,44 @@ export class Rook extends Piece {
         const dark = "https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg";
         super(player, (player === 1 ? light : dark));
     }
+
+    isMovePossible(current, destination, squares) {
+        return this.isMoveLegal(current, destination) && this.isPathPossible(current, destination, squares);
+    }
+
+    isMoveLegal(start, end) {
+        let mod = start % 8;
+        let diff = 8 - mod;
+        return (Math.abs(start-end) % 8 === 0 || (end >= (start-mod) && end < (start+diff)));
+    }
+
+    isPathPossible(start, end, squares) {
+        let pathStart, pathEnd, increment;
+        if (start > end) {
+            pathStart = end;
+            pathEnd = start;
+        } else {
+            pathStart = start;
+            pathEnd = end;
+        }
+
+        if (Math.abs(start-end) % 8 === 0) {
+            increment = 8;
+            pathStart += increment;
+        } else {
+            increment = 1;
+            pathStart += increment;
+        }
+
+        for (let i=pathStart; i<pathEnd; i+=increment) {
+            if (squares[i]) {
+                return false;
+            }
+        }
+        
+        if (squares[end]) {
+            return squares[end].player !== this.player;
+        }
+        return true;
+    }
 }
