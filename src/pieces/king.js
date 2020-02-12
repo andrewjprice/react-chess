098ff10a9be49fb88e1e6b78aac7b4e2a344c1f2
Andrew <andrew.j.price@outlook.com>
2020-02-11 20:52:12 -0800

    Add King move logic

--- src/pieces/king.js
+++ src/pieces/king.js
@@ -6,4 +6,20 @@ export class King extends Piece {
         const dark = "https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg";
         super(player, (player === 1 ? light : dark));
     }
+
+    isMovePossible(current, destination, squares) {
+        return this.isMoveLegal(current, destination) && this.isPathPossible(current, destination, squares);
+    }
+
+    isMoveLegal(start, end) {
+        let diff = Math.abs(start-end);
+        return (diff === 8 || diff === 1);
+    }
+
+    isPathPossible(start, end, squares) {
+        if (squares[end]) {
+            return squares[end].player !== this.player;
+        }
+        return true;
+    }
 }
