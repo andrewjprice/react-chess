2ebc25382686582d10cc96cc5452d74e788314e8
Andrew <andrew.j.price@outlook.com>
2020-02-15 11:34:52 -0800

    Display captured pieces

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Board from './board';
 import initializeBoard from '../helpers/initializeBoard';
+import Captured from './captured';
 import '../styles/index.css';
 
 export default class Game extends React.Component {
@@ -9,13 +10,19 @@ export default class Game extends React.Component {
     this.state = {
       squares: initializeBoard(props.player),
       currentSelected: -1,
-      player: props.player
+      player: props.player,
+      capturedWhite: [],
+      capturedBlack: []
     }
   }
 
   movePiece(i) {
     const squares = this.state.squares.slice();
+    const capturedWhite = this.state.capturedWhite.slice();
+    const capturedBlack = this.state.capturedBlack.slice();
+
     var current = squares[this.state.currentSelected];
+
     if (this.state.currentSelected === -1) {
       if (squares[i] && squares[i].player === this.state.player) {
         squares[i].style = {...squares[i].style, backgroundColor: 'gold'};
@@ -26,10 +33,17 @@ export default class Game extends React.Component {
       current.style = {...current.style, backgroundColor: ''};
 
       if (current.isMovePossible(this.state.currentSelected, i, squares)) {
+        if (squares[i]) {
+          if (squares[i].player === 1) {
+            capturedWhite.push(squares[i]);
+          } else {
+            capturedBlack.push(squares[i]);
+          }
+        }
         squares[i] = current;
         squares[this.state.currentSelected] = null;
       }
-      this.setState({ squares: squares, currentSelected: -1});
+      this.setState({ squares: squares, currentSelected: -1, capturedWhite: capturedWhite, capturedBlack: capturedBlack });
     }
   }
 
@@ -41,6 +55,9 @@ export default class Game extends React.Component {
             squares={this.state.squares}
             onClick={(i) => this.movePiece(i)}
             />
+        </div>
+        <div>
+          <Captured white={this.state.capturedWhite} black={this.state.capturedBlack} />
         </div>
       </div>
     )
--- src/components/captured.js
+++ src/components/captured.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import Square from 'square';
+import Square from './square';
+import '../styles/index.css';
 
 export default class Captured extends React.Component {
+    renderSquare(pieces) {
+        var row = [];
+        pieces.map((square) => {
+            row.push(<Square style={square.style} />);
+        });
+        return row;
+    }
     render() {
         return(
-            <div>
-                <div>{/* white */}</div>
-                <div>{/* black */}</div>
+            <div className="captured-block">
+                <div>{this.renderSquare(this.props.black)}</div>
+                <div>{this.renderSquare(this.props.white)}</div>
             </div>
         );
     }
