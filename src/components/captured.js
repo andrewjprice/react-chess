9c8774e4a3640f77f96f4aeba9bbb63ea2eb4391
Andrew <andrew.j.price@outlook.com>
2020-02-14 22:41:50 -0800

    Add captured component

---
+++ src/components/captured.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Square from 'square';
+
+export default class Captured extends React.Component {
+    render() {
+        return(
+            <div>
+                <div>{/* white */}</div>
+                <div>{/* black */}</div>
+            </div>
+        );
+    }
+}
