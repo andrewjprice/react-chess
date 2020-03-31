813da7dd228167a000971676788362a1597f0755
Andrew <andrew.j.price@outlook.com>
2020-03-30 22:13:08 -0700

    Add rank and file mask bitboards

--- src/bitboard/board.js
+++ src/bitboard/board.js
@@ -3,14 +3,14 @@ import { rankBB, idxBB } from './boards';
 /* bitboard board definition */
 export default class BoardState {
     constructor() {
-        this.whitePawns = rankBB().shl(8);
+        this.whitePawns = rankBB(1);
         this.whiteRooks = idxBB(0).or(idxBB(7));
         this.whiteKnights = idxBB(1).or(idxBB(6));
         this.whiteBishops = idxBB(2).or(idxBB(5));
         this.whiteQueen = idxBB(3);
         this.whiteKing = idxBB(4);
 
-        this.blackPawns = rankBB().shl(48);
+        this.blackPawns = rankBB(6);
         this.blackRooks = idxBB(56).or(idxBB(63));
         this.blackKnights = idxBB(57).or(idxBB(62));
         this.blackBishops = idxBB(58).or(idxBB(61));
--- src/bitboard/boards.js
+++ src/bitboard/boards.js
@@ -4,18 +4,43 @@ export function makeBB(lower, upper) {
     return new BitBoard(lower, upper);
 }
 
+/* Zero bitboard */
 export function emptyBB() {
     return makeBB(0,0);
 }
 
+/* One bitboard */
 export function oneBB() {
     return makeBB(0xffffffff, 0xffffffff);
 }
 
-export function rankBB() {
-    return makeBB(0xff, 0);
-}
-
+/* single bit bitboard */
 export function idxBB(i) {
     return makeBB(0,0).setBit(i);
+}
+
+/* rank masks */
+export function rankBB(i) {
+    return makeBB(0xff, 0).shl(i * 8);
+}
+
+export function rankMasks() {
+    let ranks = []
+    for (let i=0; i<8; i++) {
+        ranks.push(rankBB(i))
+    }
+    return ranks;
+}
+
+/* file masks */
+export function fileBB(i) {
+    return makeBB(0x01010101, 0x01010101).shl(i);
+}
+
+export function fileMasks() {
+    let files = []
+    for (let i=0; i<8; i++) {
+        files.push(fileBB(i))
+    }
+    return files;
 }
